e0affe6a28c4d553904042c998559693fae899ce
LorranIFBA <150835182+LorranIFBA@users.noreply.github.com>
2023-12-17 18:53:47 -0300

    Update main.py

--- Projeto 3/main.py
+++ Projeto 3/main.py
@@ -10,6 +10,16 @@ def recursive_xor(pt, key, encoded=None, index=0):
       return bytes(encoded)
 
 
+def repeated_key_xor(plain_text, key):
+
+  # returns plain text by repeatedly xoring it with key
+  pt = plain_text
+  len_key = len(key)
+  encoded = []
+
+  for i in range(0, len(pt)):
+    encoded.append(pt[i] ^ key[i % len_key])
+  return bytes(encoded)
 
 plain_text = b'Esta e uma mensagem codificada com o algoritmo XOR'
 key = b'SenhaXOR'
